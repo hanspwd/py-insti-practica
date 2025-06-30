2ea0abf9d9509c6661eca2ce1ec1dc9a5a39e13c
Hans Morales <hwnss.w@gmail.com>
2025-06-30 00:07:35 -0400

    Mejoras en las validaciones de listeo de promedios y por edad. Tambien se agrego la funcionalidad de eliminar aventureros.

--- prac_evaluacion3.py
+++ prac_evaluacion3.py
@@ -5,7 +5,7 @@ def registrar_aventurero(lst_aventureros: dict, codigo: str, datos: list) -> boo
         print(f"[ERROR] El codigo {codigo} ya existe, intentelo nuevamente.")
         return False
     if datos[1] < 0:
-        print("[ERROR] Edad ingresada no valida, tiene que ser mayor a 0.")
+        print("[ERROR] Edad ingresada no valida, tiene que ser mayor a cero.")
         return False
     
     lst_aventureros[codigo] = {
@@ -56,32 +56,63 @@ def modificar_puntaje(lst_aventureros: dict, codigo: str, sesion: int, nuevo_pun
 
 def mostrar_participacion(lst_aventureros: dict):
     if not lst_aventureros:
-        print("[ERROR] Asegurese de que haya al menos un aventurero registrado para listar.")
+        print("[ERROR] Asegúrese de que haya al menos un aventurero registrado para listar.")
         return False
-    for codigo, datos in lst_aventureros.items():   
-        puntaje = datos["puntajes"]
-        total = sum(puntaje)
-        promedio = total / len(puntaje)
-        print("----------------------------------------")
-        print(f"Codigo: {codigo}\nNombre: {datos["nombre"]}\nPuntaje Total: {total}\nPromedio: {promedio}")
-    print("----------------------------------------")
+
+    advertencias = ""
+
+    for codigo, datos in lst_aventureros.items():
+        puntajes = datos["puntajes"]
+
+        if not puntajes:
+            advertencias += f"[Advertencia] Aventurero {codigo} no tiene puntajes aún, por lo tanto no tiene promedio.\n".strip()
+            continue
+
+        total = sum(puntajes)
+        promedio = total / len(puntajes)
+
+        print(f"------------------------------------------")
+        print(f"Código: {codigo}")
+        print(f"Nombre: {datos['nombre']}")
+        print(f"Puntaje Total: {total}")
+        print(f"Promedio: {promedio:.2f}")
+
+    if advertencias:
+        print(f"------------------------------------------")
+        print(advertencias)
+
+    print(f"------------------------------------------")
     return True
 
 def participantes_con_bajo_promedio(lst_aventureros: dict, umbral: float):
-
+    advertencias = ""
     if not lst_aventureros:
         print("[ERROR] No hay aventureros para listar por umbral.")
+        return False
 
-    for codigo, datos in lst_aventureros.items ():
-        puntaje = datos["puntajes"]
-        promedio = sum(puntaje) / len(puntaje)
-        if promedio < umbral:
-            print("----------------------------------------")
-            print(f"Codigo: {codigo}\nNombre: {datos["nombre"]}\nPromedio: {promedio}")
-        else:
+    for codigo, datos in lst_aventureros.items():
+        puntajes = datos["puntajes"]
+
+        if not puntajes:
+            advertencias += f"[Advertencia] Aventurero {codigo} no tiene puntajes aún, por lo tanto no tiene promedio.\n".strip()
             continue
-    print("----------------------------------------")
+
+        promedio = sum(puntajes) / len(puntajes)
+
+        if promedio <= umbral:
+            print(f"------------------------------------------")
+            print(f"Código: {codigo}")
+            print(f"Nombre: {datos['nombre']}")
+            print(f"Edad: {datos['edad']}")
+            print(f"Promedio: {promedio:.2f}")
+
+    if advertencias:
+        print("------------------------------------------")
+        print(advertencias)
+
+    print(f"------------------------------------------")
     return True
+    
 
 def listar_aventureros(lst_aventureros: dict):
     if not lst_aventureros:
@@ -89,20 +120,41 @@ def listar_aventureros(lst_aventureros: dict):
         return False
     
     for codigo, datos in lst_aventureros.items():
-        print("----------------------------------------")
+        print(f"------------------------------------------")
         print(f"Codigo: {codigo}\nNombre: {datos["nombre"]}\nEdad: {datos["edad"]}\nPuntajes: {datos["puntajes"]}")
 
-    print("----------------------------------------")
+    print(f"------------------------------------------")
     return True
 
 def obtener_aventureros_por_edad(lst_aventureros: dict, edad: int):
     for codigo, datos in lst_aventureros.items():
+
+        if edad == datos["edad"]:
+            print(f"------------------------------------------")
+            print(f"Código: {codigo}")
+            print(f"Nombre: {datos['nombre']}")
+            print(f"Edad: {datos['edad']}")
+            print(f"Puntajes: {datos['puntajes']}")
+
         if edad != datos["edad"]:
             print("[ERROR] La edad ingresada no es valida o no existe en nuestros registros")
             continue
+        
+    print(f"------------------------------------------")
+    return True
+        
+def eliminar_aventurero(lst_aventureros: dict, codigo: str, confirmacion: bool):
+        if codigo not in lst_aventureros:
+            print("[ERROR] El codigo ingresado no existe, vuelva a intentarlo.")
+            return False
+        
+        if confirmacion:
+            lst_aventureros.pop(codigo)
+            print("* Aventurero eliminado con exito!")
         else:
-            print(f"Codigo: {codigo}\nNombre: {datos["nombre"]}\nEdad: {datos["edad"]}\nPuntajes: {datos["puntajes"]}")
-            return True
+            print("* Se ha decidido cancelar la eliminacion del aventurero.")
+            
+        return True
 
 activo = True
 while activo:
@@ -114,34 +166,49 @@ while activo:
         print("5.- Mostrar los aventureros con promedio por bajo del umbral.")
         print("6.- Listar todos los aventureros con sus datos y puntajes.")
         print("7.- Filtrar los aventureros por edad específica.")
+        print("8.- Eliminar un aventurero.")
         print("0.- Cerrar programa.")
-        opcion = int(input("Seleccione una opcion del 0 al 7: "))
+        opcion = int(input("- Seleccione una opcion del 0 al 8: "))
 
         if opcion == 1:
-            codigo = input("Codigo: ")
-            nombre = input("Nombre: ")
-            edad = int(input("Edad: "))
+            codigo = input("- Codigo: ").lower()
+            nombre = input("- Nombre: ")
+            edad = int(input("- Edad: "))
             datos = [nombre, edad]
             registrar_aventurero(aventureros, codigo, datos)
         elif opcion == 2:
-            codigo = input("Codigo: ")
-            puntajeAgregar = int(input("Puntaje a agregar: "))
+            codigo = input("- Codigo: ").lower()
+            puntajeAgregar = int(input("- Puntaje a agregar: "))
             registrar_puntaje(aventureros, codigo, puntajeAgregar)
         elif opcion == 3:
-            codigo = input("Codigo del aventurero: ")
-            sesion = int(input("Sesion a cambiar: "))
-            nuevoPuntaje = int(input("Ingrese el nuevo puntaje: "))
+            codigo = input("- Codigo: ").lower()
+            sesion = int(input("- Sesion a cambiar: "))
+            nuevoPuntaje = int(input("- Ingrese el nuevo puntaje: "))
             modificar_puntaje(aventureros, codigo, sesion, nuevoPuntaje)
         elif opcion == 4:
             mostrar_participacion(aventureros)
         elif opcion == 5:
-            umbral = float(input("Ingrese el umbral de promedio: "))
+            umbral = float(input("- Ingrese el umbral de promedio: "))
             participantes_con_bajo_promedio(aventureros, umbral)
         elif opcion == 6:
             listar_aventureros(aventureros)
         elif opcion == 7:
-            edad = int(input("Filtrar por edad: "))
+            edad = int(input("- Filtrar por edad: "))
             obtener_aventureros_por_edad(aventureros, edad)
+        elif opcion == "8":
+            codigo = input("- Código del aventurero a eliminar: ").lower()
+            while True:
+                confirmacion = input("- ¿Confirma esta operación? Escriba 'si' o 'no': ").lower()
+                if confirmacion == "si":
+                    confirmacionBool = True
+                    break
+                elif confirmacion == "no":
+                    confirmacionBool = False
+                    break
+                else:
+                    print("[ERROR] Valor inválido. Por favor, escriba exactamente 'si' o 'no'.")
+
+                    eliminar_aventurero(aventureros, codigo, confirmacionBool)
         elif opcion == 0:
             activo = False
             print("* Programa cerrado con exito.")
